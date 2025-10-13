6fdd1001148b33509d4eed69c213fe5ab354ffbf
Gleidson <Gleidbrianmuniz@gmail.com>
2025-10-13 15:50:59 -0300

    rota adicionar filme criado

--- back-end/api.py
+++ back-end/api.py
@@ -1,1 +1,18 @@
-import streamlit
+from fastapi import FastAPI
+import funcao
+# python -m uvicorn api:app --reload
+#Testar api Fastapi
+# /docs > Documentação Swagger 
+# /redoc > Documentação
+#inciar o fastapi
+app = FastAPI(title="Gerenciador de Filmes")
+
+
+@app.get("/")
+def home():
+    return {"mensagem": "Quero café prof"}
+
+@app.post("/filmes")
+def criar_filmes(titulo:str, genero:str , ano:int , avaliacao:float):
+    funcao.inserir_filme(titulo, genero, ano, avaliacao)
+    return {"mensagem": "Filme adcionado com sucesso!"}
